d26024bc057bbbee49145954727871cc081e264f
nataliie01 <natalie.2001.eriksson@hotmail.com>
2026-05-11 10:39:32 +0200

    Added graph in HSWFC
    Added graph in HSWFC

--- HSWFC.py
+++ HSWFC.py
@@ -1,8 +1,14 @@
 import networkx as nx
+from matplotlib import pyplot as plt
 
-graph = nx.DiGraph()
+dag = nx.DiGraph()
+dag.add_edges_from([("root", "village"), ("root", "castle"), ("root", "land"), ("village", "house"), ("village", "road"), ("castle", "road"), ("land", "road"), ("land", "water"), ("house", "22"), ("house", "6"), ("house", "6"), ("house", "8"), ("house", "23"), ("road", "23"), ("road", "7"), ("road", "9"), ("house", "24"), ("road", "24"), ("water", "24"), ("road", "0"), ("castle", "0"), ("road", "1"), ("castle", "1"), ("road", "2"), ("castle", "2"), ("road", "5"), ("castle", "5"), ("road", "12"), ("castle", "12"), ("road", "14"), ("castle", "14"), ("road", "26"), ("castle", "26"), ("water", "26"), ("castle", "3"), ("castle", "4"), ("castle", "10"), ("castle", "11"), ("castle", "13"), ("castle", "15"), ("road", "21"), ("water", "21"), ("road", "25"), ("water", "25"), ("castle", "18"), ("water", "18"), ("castle", "20"), ("water", "20"), ("water", "16"), ("water", "17"), ("water", "19")])
 
-
+print(nx.is_directed_acyclic_graph(dag))
+plt.tight_layout()
+nx.draw_networkx(dag, arrows=True)
+plt.savefig("dag.png", format="PNG")
+plt.clf()
 terminalTiles = []
 metaTiles = []
 
