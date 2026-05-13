03ca17017fddb3b22a0d1660b1813cfbd7a94547
Anna Likhanova <annet.likhanova@gmail.com>
2026-05-13 14:12:57 +0200

    Format the code, remove commented out code

--- HSWFC.py
+++ HSWFC.py
@@ -6,13 +6,19 @@ import random
 
 
 dag = nx.DiGraph()
-dag.add_edges_from([("root", "village"), ("root", "castle"), ("root", "land"), ("village", "house"), ("village", "road"), ("castle", "road"), ("land", "road"), ("land", "water"), ("house", "22"), ("house", "6"), ("house", "6"), ("house", "8"), ("house", "23"), ("road", "23"), ("road", "7"), ("road", "9"), ("house", "24"), ("road", "24"), ("water", "24"), ("road", "0"), ("castle", "0"), ("road", "1"), ("castle", "1"), ("road", "2"), ("castle", "2"), ("road", "5"), ("castle", "5"), ("road", "12"), ("castle", "12"), ("road", "14"), ("castle", "14"), ("road", "26"), ("castle", "26"), ("water", "26"), ("castle", "3"), ("castle", "4"), ("castle", "10"), ("castle", "11"), ("castle", "13"), ("castle", "15"), ("road", "21"), ("water", "21"), ("road", "25"), ("water", "25"), ("castle", "18"), ("water", "18"), ("castle", "20"), ("water", "20"), ("water", "16"), ("water", "17"), ("water", "19"), ("house", "27")])
+dag.add_weighted_edges_from([("root", "village", 0.4), ("root", "castle", 0.2), ("root", "land", 0.4), ("village", "house", 0.6), ("village", "road", 0.4), ("castle", "road", 0.4), ("land", "road", 0.6), ("land", "water", 0.4), ("house", "22", 0.1), ("house", "6", 0.1), ("house", "6", 0.1), ("house", "8", 0.1), ("house", "23", 0.1), ("road", "23", 0.1), ("road", "7", 0.1), ("road", "9", 0.1), ("house", "24", 0.1), ("road", "24", 0.1), ("water", "24", 0.1), ("road", "0", 0.1), ("castle", "0", 0.1), ("road", "1", 0.1), ("castle", "1", 0.1), ("road", "2", 0.1), ("castle", "2", 0.1), ("road", "5", 0.1), ("castle", "5", 0.1), ("road", "12", 0.1), ("castle", "12", 0.1), ("road", "14", 0.1), ("castle", "14", 0.1), ("road", "26", 0.1), ("castle", "26", 0.1), ("water", "26", 0.1), ("castle", "3", 0.1), ("castle", "4", 0.1), ("castle", "10", 0.1), ("castle", "11", 0.1), ("castle", "13", 0.1), ("castle", "15", 0.1), ("road", "21", 0.1), ("water", "21", 0.1), ("road", "25", 0.1), ("water", "25", 0.1), ("castle", "18",0.1), ("water", "18", 0.1), ("castle", "20", 0.1), ("water", "20", 0.1), ("water", "16", 0.1), ("water", "17", 0.1), ("water", "19", 0.1), ("house", "27", 0.1)])
+
+plt.tight_layout()
+nx.draw_networkx(dag, arrows=True)
+plt.savefig("dag.png", format="PNG")
+# tell matplotlib you're done with the plot: https://stackoverflow.com/questions/741877/how-do-i-tell-matplotlib-that-i-am-done-with-a-plot
+plt.clf()
 
 meta_tiles = ["village", "castle", "land", "road", "water", "house"]
-# adjacency_constraints = {"house":[]}
+
+
 terminal_q = Queue()
 collapsed_tiles = Queue()
-uncollapsed_q = Queue()
 
 
 for i in range(28):
@@ -24,7 +30,7 @@ for i in range(28):
 
 # DD
 RES = 128
-DIMS = (60, 6) #14x8)
+DIMS = (10, 6) #14x8)
 SCREEN = (DIMS[0]*RES, DIMS[1]*RES) 
 display = pygame.display.set_mode(SCREEN)
 '''
@@ -73,6 +79,7 @@ def socketMatch(socket, targetsocket):
     return True
 
 def metaConstraints(currentTile, neighborTile):
+    #print ("neighbortile", neighborTile)
     if dag.has_edge("house", currentTile["ID"]) and dag.has_edge("castle", neighborTile["ID"]):
         return False
     if dag.has_edge("castle", currentTile["ID"]) and dag.has_edge("house", neighborTile["ID"]):
@@ -95,7 +102,6 @@ class Tile:
         self.rect = self.img.get_rect()
         self.rect.topleft = self.x, self.y
         # attr. related to WFC
-        self.metatile = None
         self.entropy = len(metadata)
         self.potentialTiles = list(metadata) #will be updated as WFC runs
         self.collapsed = False
@@ -158,11 +164,6 @@ class Tile:
     def collapse(self):
         print("current tile:", self.c, self.r, "with entropy", self.entropy)
         self.collapsed = True
-        # self.collapsed = True
-        # collapsed_tiles.put(self)
-        # uncollapsed_tiles.remove(self)
-        # print("uncollapsed after collapse", uncollapsed_q.queue)
-        # print("Collapsed tiles:", collapsed_tiles.qsize(), collapsed_tiles.queue)
         if len(self.potentialTiles) > 1:            
             potTile = random.choice(self.potentialTiles)
             self.name = potTile["ID"]
@@ -181,8 +182,6 @@ class Tile:
             explode_neighbors(self)
         
         
-
-
 # DD. GRID
 # grid = [[TILE, ..., n=DIMS[0]], ..., n=DIMS[1]]
 # interp. 2D array of TILE
@@ -192,8 +191,7 @@ for r in range(DIMS[1]):
     for c in range(DIMS[0]):
         tile = Tile(c, r)
         row.append(tile)
-        uncollapsed_q.put(tile)
-        # print("uncollapsed tiles_grid", uncollapsed_q.queue)
+        # uncollapsed_q.put(tile)
     grid.append(row)
 
 # TEMPLATE FOR GRID
@@ -205,8 +203,6 @@ for r in range(DIMS[1]):
 # CODE
 
 grid[0][0].collapse()
-# grid[0][0].check_meta_constraints()
-# print("predecessors of 26:", list(dag.predecessors("26")))
 
 def draw():
     display.fill("#1e1e1e")
@@ -230,7 +226,6 @@ def update():
     for row in grid:
         for tile in row:
             if not tile.collapsed:
-                # tile.check_meta_constraints()
                 lowestEntropy = tile.updateEntropy(lowestEntropy)
 
     candidates = []
@@ -238,11 +233,10 @@ def update():
         for tile in row:
             if not tile.collapsed and tile.entropy == lowestEntropy:
                 candidates.append(tile)
-    # print("candidates", len(candidates))
+   
     if len(candidates) > 0:
-        random.choice(candidates).collapse() # Can change to random, or pick the last
-            
-    # propagation(collapsed_tiles, lowestEntropy)
+        random.choice(candidates).collapse() 
+
     
 def explode_neighbors(tile):
     print("exploding neighbors of tile", tile.c, tile.r)   
@@ -251,63 +245,29 @@ def explode_neighbors(tile):
             print("exploding right neighbor", grid[tile.r][tile.c+1].c, grid[tile.r][tile.c+1].r)
             grid[tile.r][tile.c+1].collapsed = False
             grid[tile.r][tile.c+1].potentialTiles = list(metadata)
-            # collapsed_tiles.get(grid[tile.r][tile.c+1])
-            # grid[tile.r][tile.c+1].updateEntropy(lowestEntropy)
+
      
     if tile.DOWN_neigh["COLLAPSED"]==True:                    
         if tile.r < DIMS[1]-1:   
             print("exploding down neighbor", grid[tile.r+1][tile.c].c, grid[tile.r+1][tile.c].r)
             grid[tile.r+1][tile.c].collapsed = False
             grid[tile.r+1][tile.c].potentialTiles = list(metadata)
-            # grid[tile.r+1][tile.c].updateEntropy(lowestEntropy)
-            # uncollapsed_tiles.append(grid[tile.r+1][tile.c])
+
     
     if tile.LEFT_neigh["COLLAPSED"]==True: 
         if tile.c > 0:       
             print("exploding left neighbor", grid[tile.r][tile.c-1].c, grid[tile.r][tile.c-1].r)
             grid[tile.r][tile.c-1].collapsed = False
             grid[tile.r][tile.c-1].potentialTiles = list(metadata)
-            # grid[tile.r][tile.c-1].updateEntropy(lowestEntropy)
-            # uncollapsed_tiles.append(grid[tile.r][tile.c-1])
+
     
     if tile.UP_neigh["COLLAPSED"]==True:
         if tile.r > 0:
             print("exploding up neighbor", grid[tile.r-1][tile.c].c, grid[tile.r-1][tile.c].r)
             grid[tile.r-1][tile.c].collapsed = False
             grid[tile.r-1][tile.c].potentialTiles = list(metadata)
-            # grid[tile.r-1][tile.c].updateEntropy(lowestEntropy)
-            # uncollapsed_tiles.append(grid[tile.r-1][tile.c])
 
-
-# def depropagation(list_uncollapsed_tiles):
-#     P = []
-#     while len(list_uncollapsed_tiles) > 0:
-#         tile = list_uncollapsed_tiles[0]
-#         list_uncollapsed_tiles.remove(tile)
-#         for neighbor in [tile.RIGHT_neigh, tile.DOWN_neigh, tile.LEFT_neigh, tile.UP_neigh]:
-#             pre = []
-#             post = []
-#             if neighbor == tile.RIGHT_neigh:
-#                 # print("right neighbor")               
-#                 if tile.c < DIMS[0]-1:
-#                     pre = grid[tile.r][tile.c+1].potentialTiles   
-#                     post = neighbor["ID"]         
-#             if neighbor == tile.DOWN_neigh:
-#                 if tile.r < DIMS[1]-1:
-#                         pre = grid[tile.r+1][tile.c].potentialTiles
-#                         post = neighbor["ID"] 
-#             if neighbor == tile.LEFT_neigh:
-#                 if tile.c > 0:
-#                     pre = grid[tile.r][tile.c-1].potentialTiles
-#                     post = neighbor["ID"] 
-#             if neighbor == tile.UP_neigh:
-#                 if tile.r > 0:
-#                     pre = grid[tile.r-1][tile.c].potentialTiles
-#                     post = neighbor["ID"] 
-#             # print("pre", [tile["ID"] for tile in pre])
             
-
-
 def propagation(queue_collapsed_tiles, lowestEntropy):
     while not queue_collapsed_tiles.empty():
         tile = queue_collapsed_tiles.get()
